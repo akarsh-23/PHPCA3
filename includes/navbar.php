366940aa0633eefb93c395e30babb4ffec9a88d4
Akarsh Shrivastava <akarsh_shrivastava@yahoo.com>
2022-04-10 23:28:29 +0530

    initial commit

---
+++ includes/navbar.php
@@ -0,0 +1,8 @@
+<link href='https://fonts.googleapis.com/css?family=Hind' rel='stylesheet'>
+<div class="navbar">
+	<div class="logo_div">
+		<a href="index.php"><h1 style="font-family:'Hind'; color:#0A25C8">My Blog</h1></a>
+	</div>
+	<ul>
+	</ul>
+</div>
